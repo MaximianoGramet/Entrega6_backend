9f3a22409e78a4bfed994e8516b7b35f22950da2
Gramet Maximiliano <maxigramet03@gmail.com>
2023-12-20 21:51:05 -0300

    Your commit message

---
+++ src/routes/views.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {res.render("home.hbs");});
+router.get("/realtimeproducts", (req, res) => {res.render("products.hbs");})
+
+export default router;
